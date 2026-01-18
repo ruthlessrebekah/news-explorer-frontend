596cc290e22eb20e5129b51c0c9bc2f8e4355216
ruthlessrebekah <ruthlessrebekah@gmail.com>
2026-01-18 15:02:12 -0600

    Add correct GitHub Pages base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/ruthlessrebekah/news-explorer-frontend.git",
+  base: "/news-explorer-frontend/",
   plugins: [react()],
 });
